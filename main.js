40ab5d2d7370729d68ec7f074736d8581c905c44
Narim Park <skfla2459@gmail.com>
2024-01-28 20:05:17 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let currentSlideIndex = 0; //전역변수로 호출할 때마다 업데이트
+showSlides(); //페이지 로드 시 호출
+
+function showSlides() {
+  let slides = document.querySelectorAll('.slide');
+
+  if (slides.length === 0) {
+    console.error("슬라이드를 찾을 수 없습니다.");
+    return;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+   
+  }
+  if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+  }
+
+  slides[currentSlideIndex].style.display = 'block';
+  currentSlideIndex++;
+  setTimeout(showSlides, 4000);
+}
+
